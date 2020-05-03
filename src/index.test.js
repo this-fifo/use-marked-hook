dce216ab6d96dc7f30add284669f13fb71a44204
Filipe Herculano <fifo_@live.com>
2020-05-03 13:35:17 -0400

    Update marked to latest
    Update dev dependencies

--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,8 @@
 import { useMarked } from './'
-import { renderHook } from "@testing-library/react-hooks"
+import { renderHook } from '@testing-library/react-hooks'
 
 describe('useMarked', () => {
-  it('Receives markdown and returns html', async () => {
+  it('Receives markdown and returns html', () => {
     const { result } = renderHook(() => useMarked('# test'))
     expect(result.current).toBe('<h1>test</h1>\n')
   })
